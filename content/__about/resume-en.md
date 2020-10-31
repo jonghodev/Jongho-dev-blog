7bd07efbd4f2635c40438ec571812c8172b7fd66
jongho <jongho.dev@gmail.com>
2020-10-31 11:43:19 +0900

    First Commit

---
+++ content/__about/resume-en.md
@@ -0,0 +1,13 @@
+---
+title: 'about'
+date: 2020-10-31 01:00:00
+lang: 'en'
+---
+
+# Jongho
+
+<div align="center">
+
+_Thank you for reading my resume. If you want to contact me, Please send me an email._
+
+</div>
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Jongho Dev`,
+  description: `개발 일지`,
+  author: `Jongho`,
+  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  siteUrl: `https://www.jongho.dev`, // Your blog site url
+  social: {
+    twitter: `jonghodev`,
+    github: `jonghodev`,
+    medium: `jonghodev`,
+    facebook: `jonghodev`,
+    linkedin: `jonghodev`,
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: 'jongho', // Your disqus-short-name. check disqus.com.
+    utterances: 'jonghodev/blog', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jongho',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: 'G-W6C9JGL04T', // Add your google analytics tranking ID
+}
